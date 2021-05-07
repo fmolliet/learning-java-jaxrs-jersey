ce925833d3e962cc917d380fb36889e838f9d563
fmolliet <fmolliet@hotmail.com>
2021-05-07 15:26:25 -0300

    Corrgido testes para requests realizadas

--- src/main/java/br/com/alura/loja/jersey/Main.java
+++ src/main/java/br/com/alura/loja/jersey/Main.java
@@ -21,11 +21,8 @@ public class Main {
      */
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
-        // in br.com.alura.loja.jersey package
         final ResourceConfig rc = new ResourceConfig().packages("br.com.alura.loja.jersey");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
+        
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
